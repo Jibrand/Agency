bec548d1f9d146d98a04cb2fff21a2f0784224dc
Jibrand <muhammadjibrandev@gmail.com>
2023-11-11 14:20:36 -0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import './App.css'
 import Index from './pages/Home/Index'
 import Index1 from './pages/About/Index'
-import Index2 from './pages/Contact/Index'
-import SignIn from './pages/Authentication/Signin'
-import Signup from './pages/Authentication/Signup'
-import Dashboard from './pages/Dashboard/Dashboard'
-import Dashboard1 from './pages/Dashboard/Dashboard1'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -17,11 +12,6 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Index />} />
           <Route exact path='/about' element={<Index1 />} />
-          <Route exact path='/contact' element={<Index2 />} />
-          <Route exact path='/signin' element={<SignIn />} />
-          <Route exact path='/signup' element={<Signup />} />
-          <Route exact path='/dashboard' element={<Dashboard />} />
-          <Route exact path='/dashboard1' element={<Dashboard1 />} />
         </Routes>
       </Router>
     </>
